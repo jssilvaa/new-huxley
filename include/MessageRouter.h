d296e59d5a69312ff6c1c843b6ae759115e8ff85
jssilvaa <117296619+jssilvaa@users.noreply.github.com>
2025-11-03 11:59:34 +0000

    Add registerMessage method to MessageRouter

--- include/MessageRouter.h
+++ include/MessageRouter.h
@@ -9,8 +9,9 @@ class MessageRouter {
 public:
     MessageRouter(CryptoEngine* crypto);
 
+    void registerMessage(ClientState* state); 
     bool routeMessage(const std::string& sender, const std::string& recipient, const std::string& message);
-
+    
 private:
     CryptoEngine* cryptoEngine;
 };
